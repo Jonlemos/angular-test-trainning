e10669de73bcc4692ed0e28d174cbc664b9b2800
jonlemos <lemos.anjos@gmail.com>
2026-05-04 17:36:24 -0300

    add environments

---
+++ apps/angular-host/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  authApiUrl: 'http://localhost:3001/api',
+  chargeApiUrl: 'http://localhost:3002/api',
+  renegotiationApiUrl: 'http://localhost:3003/api',
+  enableLogging: true,
+  version: '1.0.0'
+};
